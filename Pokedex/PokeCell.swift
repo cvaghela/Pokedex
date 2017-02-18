e7329ba4afb1afbea3e7bb5316dbae0d060861d6
Chintan Vaghela <cheaterchintz@gmail.com>
2017-02-18 13:02:06 -0500

    Model & View Added
    Added PokeCell class, Pokemon class and UI Added

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Chintan Vaghela on 2/18/17.
+//  Copyright © 2017 CVBuilts. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
